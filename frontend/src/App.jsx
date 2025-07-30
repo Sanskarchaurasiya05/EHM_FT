ceb49e69837f25ee93f7ae21d04a37c8f441098e
sanskar <sanskarvns19@gmail.com>
2025-07-30 20:01:12 +0530

    WIP: local changes to App.jsx before merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,17 @@
-<<<<<<< Updated upstream
+
 import React from 'react';
 import HeroSection from './Components/Herosection/HeroSection';
-=======
 
 import './App.css'
 import Footer from './Components/Footer/Footer.jsx'
->>>>>>> Stashed changes
 
 function App() {
   return (
-<<<<<<< Updated upstream
     <div className="App">
       <HeroSection />
     </div>
   );
-=======
-    <>
-<Footer/>
-    </>
-  )
->>>>>>> Stashed changes
+
 }
 
 export default App;
